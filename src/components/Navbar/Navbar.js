77505c61554a31f953d48d7d863f52e2f7fe0853
tonytran97 <tonytran_97@live.com>
2023-03-07 18:33:18 -0800

    some navbar alignment with bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,22 +3,27 @@ import React from "react";
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="wrap">
-            </div>
-            <div className="left">
-                <div className="tab">
-                    <span>Plants</span>
+            <div className="container">
+                <div className="row justify-content-start">
+                    <div className="tab col">
+                        <span>Plants</span>
+                    </div>
+                    <div className="tab col">
+                        <span>Planters</span>
+                    </div>
+                    <div className="tab col">
+                        <span>Plant Care</span>
+                    </div>
                 </div>
-                <div className="tab">
-                    <span>Planters</span>
+                <div className="justify-content-center">
+                    <span>Tony's Plant Shop</span>
                 </div>
-                <div className="tab">
-                    <span>Plant Care</span>
+                <div className="row justify-content-end">
+                    <ul className="icon col">Homepage Icon</ul>
+                    <ul className="icon col">About</ul>
+                    <ul className="icon col">Contact</ul>
+                    <ul className="icon col">Shopping Cart Icon</ul>
                 </div>
-            </div>
-            <div className="middle">
-            </div>
-            <div className="right">
             </div>
         </div>
 
